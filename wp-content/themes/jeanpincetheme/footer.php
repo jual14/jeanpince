3deecc4ed572037121d218c9c71d2d98ccd877e2
julien <julien.alix14@gmail.com>
2017-04-14 11:00:57 +0200

    template created

---
+++ wp-content/themes/jeanpincetheme/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package jeanpincetheme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jeanpincetheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'jeanpincetheme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'jeanpincetheme' ), 'jeanpincetheme', '<a href="https://automattic.com/" rel="designer">Julien Alix</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
